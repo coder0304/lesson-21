32148646dd0975265b6d833ed3dedd3436f67acf
coder0304 <nellikatgowri@gmail.com>
2025-01-27 19:07:14 +0530

    a2.py

---
+++ a2.py
@@ -0,0 +1,6 @@
+country_code={'India':'0091','Australia':'0025','Nepal':'00977'}
+print("Countrycode for india-")
+print(country_code.get('India','Not Found'))
+print(country_code.get('India','not found'))
+print("country code for japan-")
+print(country_code.get('japan','Not Found'))
